e8fe3efc95b45035828a9d9d1913547a7b762fd6
wa2c <entry@wa2c.com>
2018-12-07 01:58:20 +0900

    - Date updater

--- src/main/java/com/wa2c/mediaupdater/MainForm.java
+++ src/main/java/com/wa2c/mediaupdater/MainForm.java
@@ -1,8 +1,16 @@
 package com.wa2c.mediaupdater;
 
 import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -25,6 +33,113 @@ public class MainForm extends JFrame {
             }
         });
         Program.Pref = Settings.readSettings();
+
+        initializeUI();
     }
 
+    private void initializeUI() {
+        mainContentPanel.setTransferHandler(new DropFileHandler());
+    }
+
+    private boolean searchFiles(File file) {
+        if (file == null || !file.exists())
+            return false;
+
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                if (searchFiles(f))
+                    break;
+            }
+            return false;
+        }
+
+        int point = file.getName().lastIndexOf(".");
+        if (point < 1) {
+            return false;
+        }
+        String ext = file.getName().substring(point + 1);
+        if (!ext.toLowerCase().equals("mp3") && !ext.toLowerCase().equals("m4a") && !ext.toLowerCase().equals("ogg"))
+            return false;
+
+        updateFolder(file.getParentFile(), file.lastModified());
+        return true;
+    }
+
+    private void updateFolder(File file, long time) {
+        if (file == null || !file.exists())
+            return;
+
+        file.setLastModified(time);
+        file.setReadOnly();
+
+        Path path = Paths.get(file.getAbsolutePath());
+        try {
+            Files.setAttribute(path, "dos:readonly", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                updateFolder(f, time);
+            }
+        }
+    }
+
+
+
+
+    /**
+     * Drag & drop handler.
+     */
+    private class DropFileHandler extends TransferHandler  {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * ドロップされたものを受け取るか判断 (ファイルのときだけ受け取る)
+         */
+        @Override
+        public boolean canImport(TransferSupport support) {
+            if (!support.isDrop()) {
+                // ドロップ操作でない場合は受け取らない
+                return false;
+            }
+
+            if (!support.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                // ドロップされたのがファイルでない場合は受け取らない
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+        /**
+         * ドロップされたファイルを受け取る
+         */
+        @Override
+        public boolean importData(TransferSupport support) {
+            // 受け取っていいものか確認する
+            if (!canImport(support)) {
+                return false;
+            }
+
+            // ドロップ処理
+            Transferable t = support.getTransferable();
+            try {
+                // ファイルを受け取る
+                @SuppressWarnings("unchecked")
+                List<File> files = (List<File>) t.getTransferData(DataFlavor.javaFileListFlavor);
+                for (File f : files) {
+                    searchFiles(f);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }
+    }
 }
